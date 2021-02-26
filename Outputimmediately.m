2b12b4d81e0fb2ac248c755a341a3142da73b050
Roman Szewczyk <rszewczyk@onet.pl>
2021-02-26 09:32:15 +0100

    Add files via upload

---
+++ Outputimmediately.m
@@ -0,0 +1,49 @@
+function result=Outputimmediately(verbouse)
+
+% The MIT License (MIT)
+%
+% Copyright (c) 2021 Roman Szewczyk
+%
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+% 
+% The above copyright notice and this permission notice shall be included in all
+% copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+% SOFTWARE.
+% 
+%
+% DESCRIPTION:
+% Configure output to print immediately to the screen.
+% 
+% AUTHOR: Roman Szewczyk, rszewczyk@onet.pl
+%
+% USAGE:
+% ChkPkg(1);    - with confirmation
+% or
+% ChkPkg(0);    - quiet mode
+% 
+
+t=ver;
+
+if strcmp(t(1).Name,'Octave')==1
+   if verbouse
+     fprintf('Output immediately to the screen. \n');
+   end
+   page_screen_output(0);
+   page_output_immediately(1);  % print immediately at the screen
+end
+
+result=1;
+
+end
