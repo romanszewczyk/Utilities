42e887cfd31cc58756e8f647830c8dbaeabbf7ea
Roman Szewczyk <rszewczyk@onet.pl>
2021-06-28 10:31:56 +0200

    Add files via upload

---
+++ draw_nice_plot_interp.m
@@ -0,0 +1,45 @@
+% MIT License
+%
+% Copyright (c) 2021 Roman Szewczyk
+%
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+%
+% The above copyright notice and this permission notice shall be included in all
+% copies or substantial portions of the Software.
+%
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+% SOFTWARE.
+
+x=-4:0.52:4.6;
+
+mu=0;
+s=1;
+
+gauss_distribution= @(x,mu,s) 1./(s.*(2.*pi).^0.5).*exp(-1./2.*((x-mu)./s).^2);
+
+y=gauss_distribution(x, mu, s);
+
+% interpolation
+
+xi=min(x):(max(x)-min(x))./100:max(x);
+yi=interp1(x,y,xi,'spline');
+
+
+plot(x,y,'sk','linewidth',4,xi,yi,'-k','linewidth',4);
+set(gca,'fontsize',24);
+ylim([0 0.6]);
+xticks(-4:1:4);
+yticks(0:0.1:0.6);
+xlabel('{\it x}');
+ylabel('{\it p(x)   for \sigma=1, \mu=0}');
+grid;
